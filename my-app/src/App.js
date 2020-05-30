98d8f675714aa4b7677bfdf0a0e86b1ae325fcd6
ashankbehara <abehara2@illinois.edu>
2020-05-30 10:19:57 -0700

    succesfully installed semantic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  
+  return(
+    <div>
+      Aloha my friends
     </div>
   );
-}
+  
+  }
 
-export default App;
+
