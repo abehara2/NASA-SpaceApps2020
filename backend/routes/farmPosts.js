d7b7b9a0cf40a45a76123d27da6bd28f51271072
Alice Fang <alicesf2@illinois.edu>
2020-05-30 17:26:47 -0500

    comment out farmPosts update endpoints

--- backend/routes/farmPosts.js
+++ backend/routes/farmPosts.js
@@ -1,14 +1,14 @@
-const {FarmPost} = require("../models")
+const { FarmPost } = require("../models");
 module.exports = function(router) {
   const farmRoutes = router.route("/farmPosts");
   const farmRoute = router.route("/farmPosts/:id");
   const farmRouteVolunteer = router.route("/farmPosts/:id/volunteers");
   const farmRouteConsumer = router.route("/farmPosts/:id/consumers");
-  
+
   const SUCCESS = 200;
   const NOT_FOUND = 404;
   const SERVER_ERR = 500;
-  
+
   //get single farmPost
   farmRoute.get(async (req, res) => {
     const { id } = req.params;
@@ -52,7 +52,15 @@ module.exports = function(router) {
 
   //post farmRoutes
   farmRoutes.post(async (req, res) => {
-    const { name, email, description, address, time, volunteers, consumers } = req.body;
+    const {
+      name,
+      email,
+      description,
+      address,
+      time,
+      volunteers,
+      consumers
+    } = req.body;
     const newPost = new FarmPost({
       name,
       email,
@@ -79,62 +87,62 @@ module.exports = function(router) {
     }
   });
 
-  //update volunteers
-  farmRouteVolunteer.put(async (req, res) => {
-    const { id } = req.params;
-    const { volunteers} = req.body;
-    const fieldsToUpdate = {};
-    if (volunteers) {
-      fieldsToUpdate["volunteers"] = volunteers;
-    }
-    try {
-      const post = await FarmPost.findByIdAndUpdate(
-        id,
-        { $set: fieldsToUpdate },
-        { new: true }
-      );
-      if (!post) {
-        res.status(NOT_FOUND).send({
-          message: "Post not found."
-        });
-      }
-      res.status(SUCCESS).send({
-        message: "Post volunteers updated successfully."
-      });
-    } catch (err) {
-      res.status(SERVER_ERR).send({
-        message: "Internal server error."
-      });
-    }
-  });
+  // //update volunteers
+  // farmRouteVolunteer.put(async (req, res) => {
+  //   const { id } = req.params;
+  //   const { volunteers} = req.body;
+  //   const fieldsToUpdate = {};
+  //   if (volunteers) {
+  //     fieldsToUpdate["volunteers"] = volunteers;
+  //   }
+  //   try {
+  //     const post = await FarmPost.findByIdAndUpdate(
+  //       id,
+  //       { $set: fieldsToUpdate },
+  //       { new: true }
+  //     );
+  //     if (!post) {
+  //       res.status(NOT_FOUND).send({
+  //         message: "Post not found."
+  //       });
+  //     }
+  //     res.status(SUCCESS).send({
+  //       message: "Post volunteers updated successfully."
+  //     });
+  //   } catch (err) {
+  //     res.status(SERVER_ERR).send({
+  //       message: "Internal server error."
+  //     });
+  //   }
+  // });
 
-  //update volunteers
-  farmRouteConsumer.put(async (req, res) => {
-    const { id } = req.params;
-    const { consumers } = req.body;
-    const fieldsToUpdate = {};
-    if (consumers) {
-      fieldsToUpdate["consumers"] = consumer;
-    }
-    try {
-      const post = await FarmPost.findByIdAndUpdate(
-        id,
-        { $set: fieldsToUpdate },
-        { new: true }
-      );
-      if (!post) {
-        res.status(NOT_FOUND).send({
-          message: "Post not found."
-        });
-      }
-      res.status(SUCCESS).send({
-        message: "Post consumers updated successfully."
-      });
-    } catch (err) {
-      res.status(SERVER_ERR).send({
-        message: "Internal server error."
-      });
-    }
-  });
+  // //update volunteers
+  // farmRouteConsumer.put(async (req, res) => {
+  //   const { id } = req.params;
+  //   const { consumers } = req.body;
+  //   const fieldsToUpdate = {};
+  //   if (consumers) {
+  //     fieldsToUpdate["consumers"] = consumer;
+  //   }
+  //   try {
+  //     const post = await FarmPost.findByIdAndUpdate(
+  //       id,
+  //       { $set: fieldsToUpdate },
+  //       { new: true }
+  //     );
+  //     if (!post) {
+  //       res.status(NOT_FOUND).send({
+  //         message: "Post not found."
+  //       });
+  //     }
+  //     res.status(SUCCESS).send({
+  //       message: "Post consumers updated successfully."
+  //     });
+  //   } catch (err) {
+  //     res.status(SERVER_ERR).send({
+  //       message: "Internal server error."
+  //     });
+  //   }
+  // });
   return router;
 };
