5b292c093becb42108fb44050ed9fc5f7b204e5d
ashankbehara <abehara2@illinois.edu>
2020-05-30 14:34:29 -0700

    FarmPost API completed

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,7 @@ var UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   destination: { type: String, required: true },
+  address : { type: String, required: true },
   time: { type: String, required: true },
   volunteerEvents: { type: [String], required: true },
   consumerEvents: { type: [String], required: true },
