b5c0df967a74ddae05525cc5af10364a31a54a6c
ashankbehara <abehara2@illinois.edu>
2020-05-30 16:48:53 -0700

    string

--- frontend/src/ProfileEvents.js
+++ frontend/src/ProfileEvents.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import {Grid, Button, Card, Icon, Image } from 'semantic-ui-react';
-import {getUser} from "./utils/apiWrapper;
+import {getUser} from "../utils/apiWrapper";
 
 export default function ProfileEvents() {
     let name = getUser("5ed2d2d2b81278eceffe1fe2")
