2a05f3eb82f2be9a93922437409bd77e760a0873
Alice Fang <alicesf2@illinois.edu>
2020-05-30 16:42:00 -0500

    post and get working for users

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,10 +5,10 @@ var UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   destination: { type: String, required: true },
-  time: { type: String, required: true },
-  volunteerEvents: { type: [String], required: true },
-  consumerEvents: { type: [String], required: true },
-  description: { type: String, required: false }
+  time: { type: String, default: "" },
+  volunteerEvents: { type: [String], default: [] },
+  consumerEvents: { type: [String], default: [] },
+  description: { type: String, default: "" }
 });
 
 // Export the Mongoose model
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,72 @@
+const { User } = require("../models");
+
 module.exports = function(router) {
+  const usersRoute = router.route("/users");
+  const userRoute = router.route("/users/:id");
+
+  const SUCCESS = 200;
+  const NOT_FOUND = 404;
+  const SERVER_ERR = 500;
+  const SERVER_ERR_MSG = "Internal server error.";
+
+  // create new User
+  usersRoute.post(async (req, res) => {
+    const {
+      name,
+      email,
+      destination,
+      time,
+      volunteerEvents,
+      consumerEvents,
+      description
+    } = req.body;
+    const newUser = new User({
+      name,
+      email,
+      destination,
+      time,
+      volunteerEvents,
+      consumerEvents,
+      description
+    });
+    try {
+      const user = await newUser.save();
+      if (user) {
+        res.status(SUCCESS).send({
+          message: "Successfully created new user."
+        });
+      } else {
+        res.status(NOT_FOUND).send({
+          message: "User not found."
+        });
+      }
+    } catch (err) {
+      res.status(SERVER_ERR).send({
+        message: SERVER_ERR_MSG
+      });
+    }
+  });
+
+  //get specific User
+  userRoute.get(async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    try {
+      if (user) {
+        res.status(SUCCESS).send({
+          message: `${user.name} Successfully retrieved`,
+          data: user
+        });
+      } else {
+        res.status(NOT_FOUND).send({
+          message: `User not found.`
+        });
+      }
+    } catch (err) {
+      res.status(SERVER_ERR).send({
+        message: SERVER_ERR_MSG
+      });
+    }
+  });
   return router;
 };
