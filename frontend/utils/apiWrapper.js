7dd6ac2e3fc290a4b0cea21fc3df02388257fff5
Alice Fang <alicesf2@illinois.edu>
2020-05-30 18:13:39 -0500

    added more wrapper functions

--- frontend/utils/apiWrapper.js
+++ frontend/utils/apiWrapper.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL = process.env.BACKEND_URL ?? "http://localhost:4000/api";
 
-export const getUser = id => {
-  const requestString = `${BASE_URL}/users/${id}`;
+export const getUser = userId => {
+  const requestString = `${BASE_URL}/users/${userId}`;
   return axios
     .get(requestString, {
       headers: {
@@ -13,6 +13,46 @@ export const getUser = id => {
     .catch(error => {
       return {
         type: "GET_USER_FAIL",
+        error
+      };
+    });
+};
+
+export const signUpToVolunteer = (userId, farmPostToAdd, destination) => {
+  const requestString = `${BASE_URL}/users/${userId}/volunteer`;
+  return axios
+    .put(
+      requestString,
+      { farmPostToAdd, destination },
+      {
+        headers: {
+          "Content-Type": "application/JSON"
+        }
+      }
+    )
+    .catch(error => {
+      return {
+        type: "VOLUNTEER_FAIL",
+        error
+      };
+    });
+};
+
+export const signUpToConsume = (userId, farmPostToAdd) => {
+  const requestString = `${BASE_URL}/users/${userId}/consumer`;
+  return axios
+    .put(
+      requestString,
+      { farmPostToAdd },
+      {
+        headers: {
+          "Content-Type": "application/JSON"
+        }
+      }
+    )
+    .catch(error => {
+      return {
+        type: "CONSUMER_FAIL",
         error
       };
     });
