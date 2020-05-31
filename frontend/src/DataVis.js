3588fc961d74c6d4539081ac54af0f79580f44d3
niviruw <niviruw@yahoo.com>
2020-05-31 15:32:56 -0700

    datavis styling

--- frontend/src/DataVis.js
+++ frontend/src/DataVis.js
@@ -10,16 +10,24 @@ import Poverty from './Poverty';
 export default function DataVis() {
     return (
         <div id="container">
-             <div id="crop">
-                <Crop />
-             </div>
-             <div id="poverty">
-            <Poverty />
-            </div>
-            <div id="text">
-                <p>These graphs present a visual aspect to the effect COVID-19 has had on our country. As seen in the graph at left, there has been a drop in price for all major crops. 
-                    According to the Law of Supply and Demand we can infer that <b style={{color: "#FF5A60"}}>demand has increased at a constant supply</b> thus we can see that a <b style={{color: "#FF5A60"}}>surplus</b> has formed. </p>
-            </div>
+            <Grid.Row>
+                <Grid.Column width={8}>
+                    <div id="crop">
+                        <Crop />
+                    </div>
+                </Grid.Column>
+                <Grid.Column width={8}>
+                    <div id="poverty">
+                        <Poverty />
+                    </div>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row centered>
+                <div id="text">
+                    <p>These graphs present a visual aspect to the effect COVID-19 has had on our country. As seen in the graph at left, there has been a drop in price for all major crops. 
+                        According to the Law of Supply and Demand we can infer that <b style={{color: "#FF5A60"}}>demand has increased at a constant supply</b> thus we can see that a <b style={{color: "#FF5A60"}}>surplus</b> has formed. </p>
+                </div>
+            </Grid.Row>
         </div>
     );
 }
