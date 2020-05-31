6f91acaee9627a3f2299173cafb3beb403e22c33
ashankbehara <abehara2@illinois.edu>
2020-05-30 17:02:19 -0700

    updated use

--- frontend/src/ProfileEvents.js
+++ frontend/src/ProfileEvents.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import {Grid, Button, Card, Icon, Image } from 'semantic-ui-react';
