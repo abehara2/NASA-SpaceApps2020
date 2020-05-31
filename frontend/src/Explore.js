ab365ccb980868f8b750b79d969d3e52dbf6e118
ashankbehara <abehara2@illinois.edu>
2020-05-31 15:42:31 -0700

    fix merge conflicts

--- frontend/src/Explore.js
+++ frontend/src/Explore.js
@@ -4,18 +4,13 @@ import "semantic-ui-css/semantic.min.css";
 import { Grid, Button, Card, Icon, Image } from "semantic-ui-react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Notiflix from "notiflix";
-<<<<<<< HEAD
-// import MapContainer from "./MapContainer";
-const place = "Archbishop Mitty";
-=======
 import MapContainer from "./MapContainer";
 const place = "Gingrich Farms Illinois";
->>>>>>> b3b7bd3b900c6c92df08038a699ed15739453947
 
 export default function Explore() {
   return (
     <div>
-      {/* <iframe
+       <iframe
         className="map"
         src={`https://maps.google.com/maps?q=${place}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
         style={{
@@ -27,15 +22,9 @@ export default function Explore() {
           borderRadius: "1%"
         }}
       >
-<<<<<<< HEAD
-        <MapContainer style={{ float: "center" }} />
-      </iframe> */}
-      <Button
-=======
         {/* <MapContainer style={{ float: "center" }} /> */}
       </iframe>
       {/* <Button
->>>>>>> b3b7bd3b900c6c92df08038a699ed15739453947
         animated
         style={{ width: "10%" }}
         onClick={() => Notiflix.Notify.Success("Just Testing Notiflix")}
